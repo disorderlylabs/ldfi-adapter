20a41c11a92dc6f681c664b37d89d698c9e8579c
Peter Alvaro <palvaro@ucsc.edu>
2019-08-20 20:18:42 -0700

    test

---
+++ bookinfo/concurrent.sh
@@ -0,0 +1,11 @@
+sh cheapunit.sh > 1.out &
+sh cheapunit.sh details > 1d.out &
+sh cheapunit.sh > 2.out &
+sh cheapunit.sh > 3.out &
+sh cheapunit.sh ratings > 4r.out &
+sh cheapunit.sh  > 4.out &
+sh cheapunit.sh ratings details > 5rd.out &
+sh cheapunit.sh > 5.out &
+sh cheapunit.sh > 6.out &
+sh cheapunit.sh > 7.out &
+sh cheapunit.sh > 6.out &
