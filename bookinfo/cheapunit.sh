0f9fa3460cc2120ce0d6621961bd6cb3c1e59eeb
Peter Alvaro <palvaro@ucsc.edu>
2019-08-20 20:14:28 -0700

    yay

--- bookinfo/cheapunit.sh
+++ bookinfo/cheapunit.sh
@@ -2,28 +2,42 @@
 HEADERS="-H x-ebay-ldfi:fail=ratings"
 #HEADERS=""
 
-curl -vvv $HEADERS http://192.168.99.103:31380/productpage > unit.out
+#HEADERS="-H x-ebay-ldfi:fail=ratings,fail=details"
+
+args="-H x-ebay-ldfi:"
+for var in "$@"; do
+    args="$args,fail=$var"
+done
+
+
+FILE=unit.$$.out
+
+
+echo args $args
+
+curl -vvv $args http://192.168.99.103:31380/productpage > $FILE
 
 
 # check if call to ratings succeeded:
-grep glyphicon-star unit.out > /dev/null 2>&1
+grep glyphicon-star $FILE > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
     echo PASS ratings
 else
     echo NO RATINGS
 fi
 
-grep English unit.out > /dev/null 2>&1
+grep English $FILE > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
     echo PASS details
 else
     echo NO DETAILS
 fi
 
-grep refreshing unit.out > /dev/null 2>&1
+grep refreshing $FILE > /dev/null 2>&1
 if [ "$?" -eq "0" ]; then
     echo PASS reviews
 else
     echo NO REVIEWS
 fi
 
+rm $FILE
